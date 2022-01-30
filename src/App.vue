e7d7dcbcf3a5fb0aca9150c1582c5aea80ceb646
Thanasis Georgiou <contact@thgeorgiou.com>
2022-01-30 11:11:35 +0200

    Make text color a bit darker

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import TheFooter from './components/TheFooter.vue';
 </script>
 
 <template>
-  <div class="bg-gray-darker text-white h-screen flex flex-col flex-nowrap overflow-x-auto">
+  <div class="bg-gray-darker text-gray-lighter h-screen flex flex-col flex-nowrap overflow-x-auto">
     <TheHeader></TheHeader>
     <router-view class="flex-1"></router-view>
     <TheFooter></TheFooter>
